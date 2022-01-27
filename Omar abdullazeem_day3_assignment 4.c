a03fd570c2e6110929553a8c05fba372a6580884
OmarAddullazeem <omar.abdullazeem@gmail.com>
2022-01-27 11:17:14 +0200

    mm

--- Omar abdullazeem_day3_assignment 4.c
+++ Omar abdullazeem_day3_assignment 4.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void main()
 {
-  int i,j,rows;
+  int rows;
   int no=1 ; // initial start for the pyramid....
   printf("Enter the number of rows of the triangle:");
   scanf("%d",&rows);
-  for(i=1; i<=rows ;i++)
+  for(int i=1; i<=rows ;i++)
   {
-    for(j=1; j<=i ;j++)
+    for(int j=1; j<=i ;j++)
     {
       if(j<=i)
       printf("%d  ",no++); // also double spaces between each number, and we cant use \n or each line will have his own line.."signature Omar.A"
@@ -15,5 +15,6 @@ void main()
     }
     printf("\n");
   }
+printf("hellowprld");
 
 }
